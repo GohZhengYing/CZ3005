434b4093d0c5d44b2a052865843beceecd5e870b
Jovian20 <92246107+Jovian20@users.noreply.github.com>
2022-09-30 20:16:08 +0800

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import json
+import queue
+from queue import PriorityQueue
+
+
+# In[2]:
+
+
+with open("Coord.json", "r") as h:
+        coord = json.load(h)
+        h.close()
+
+with open("Cost.json", "r") as i:
+        cost = json.load(i)
+        i.close()
+
+with open("Dist(2).json", "r") as j:
+        dist = json.load(j)
+        j.close()
+
+with open("G(2).json", "r") as k:
+        g = json.load(k)
+        k.close()
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+class PathData:
+
+
+    def __init__(self, path="No path found", dist=-1, energy=-1):
+
+        self.path = path
+        self.dist = dist
+        self.energy = energy
+
+
+# In[4]:
+
+
+def findminpath(source: str, dest: str, g: dict, dist: dict) -> PathData:
+    queue = PriorityQueue()
+    visited = set()
+    queue.put((0, [source]))
+    
+    while queue.not_empty:
+        
+        pair = queue.get()
+        
+        current_path = pair[1]
+        
+        current = current_path[-1]
+        
+        distance = 0
+        
+        if current not in visited:
+            visited.add(current)
+            
+            if current == dest:
+                pathinformation = PathData()
+                pathinformation.path = "->".join(current_path)
+                for i in range(len(current_path) - 1):
+                    distance += dist[f"{current_path[i]},{current_path[i+1]}"]
+                pathinformation.dist = distance
+                return pathinformation
+            
+            for neighbors in g[current]:
+                new_dist = dist[f"{current},{neighbors}"]
+                score = new_dist + pair[0]
+                
+                new_path = list(pair[1])
+                new_path.append(neighbors)
+                
+                queue.put((score, new_path))
+                
+    return PathData()
+
+
+# In[5]:
+
+
+def print_path(label: str, pathinformation: PathData) -> None:
+    print(label)
+    print(f"Shortest path: {pathinformation.path}")
+    print(f"Shortest distance: {pathinformation.dist}")
+    print(f"Total energy cost: {pathinformation.energy}")
+
+
+# In[6]:
+
+
+path = findminpath('1', '50', g, dist)
+print_path("task 1", path)
+
+
+# In[ ]:
+
+
+
+
