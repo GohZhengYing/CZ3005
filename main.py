ec198c3aec7b3929ddbe3f363366d393ca5c1f87
Lee Yih Jie <leeyihjie@yahoo.com.sg>
2022-09-30 13:19:50 +0800

    Included JSON files, Task2.py and Main,py

--- main.py
+++ main.py
@@ -1,72 +1,99 @@
-import json
-import math
+import task1
+import task2
+import task3
 
-G_file = open('../G.json','r')
-Coord_file = open('../Coord.json','r')
-Cost_file = open('../Cost.json','r')
-Dist_file = open('../Dist.json','r')
+energy_budget = 287932
 
-G = json.load(G_file)
-Coord = json.load(Coord_file)
-Cost = json.load(Cost_file)
-Dist = json.load(Dist_file)
+if __name__ == "__main__":
+    main()
 
-budget = 287932
-start = '1'
-end = '50'
+def main():
+    user_choice = input("Choose which task number to run [1 or 2 or 3]: ")
 
-def run(c):
-    global G,Coord,Cost,Dist,budget,start,end
-    path = [[start],0,0,0]
-    total_cost = 0
-    total_dist = 0
-    deadend = []
-
-    path_found = False
-    priority_queue = [[[start],0,0,0]] #[path,total_cost,total_dist,f(n)]
-
-    visited = []
-
-    while len(priority_queue) >0 and (not path_found):
-        current = priority_queue[len(priority_queue)-1]
-        priority_queue.remove(current)
-        if len(current[0])>1:
-            visited.append(current[0][-1])
-
-
-        for path_node in G[current[0][-1]]:
-            if path_node == end:
-                current[1] += Cost[current[0][-1]+','+path_node]
-                current[2] += Dist[current[0][-1] + ',' + path_node]
-                current[0].append(path_node)
-                path = current
-                path_found = True
-            elif len(G[path_node])>1 and path_node not in current[0] and current[1]+Cost[current[0][-1]+','+path_node]<=budget and path_node not in visited:
-                new_path = [current[0]+[path_node],current[1]+Cost[current[0][-1]+','+path_node],current[2]+Dist[current[0][-1]+','+path_node]]
-                h = math.sqrt((Coord[current[0][-1]][0] - Coord[path_node][0]) ** 2 + (Coord[current[0][-1]][1] - Coord[path_node][1]) ** 2)
-                new_path.append(h*c + new_path[1] + new_path[2])
-                if len(priority_queue) == 0:
-                    priority_queue.append(new_path)
-
-                else:
-                    for i in range(len(priority_queue)):
-                        if priority_queue[i][3]<=new_path[3]:
-                            priority_queue.insert(i,new_path)
-                            break
-                        elif i == len(priority_queue)-1:
-                            priority_queue.append(new_path)
+    if user_choice == 1:
+        continue
+    elif user_choice == 2:
+        task2.task2_search("1", "50", energy_budget)
+    elif user_choice == 3:
+        task3.run()
+    else:
+        print("Task not found! Exiting application!")
+        exit()
 
 
 
-    return path[2]
 
-best_c = 0
-lowest = 1000000000
-for c in range(0,30):
-    result = run(c/10)
-    if result<lowest and result>0:
-        best_c = c
-        lowest = result
 
-print(best_c,lowest)
-#96 158807.5169069521
+
+# import json
+# import math
+#
+# G_file = open('../G.json','r')
+# Coord_file = open('../Coord.json','r')
+# Cost_file = open('../Cost.json','r')
+# Dist_file = open('../Dist.json','r')
+#
+# G = json.load(G_file)
+# Coord = json.load(Coord_file)
+# Cost = json.load(Cost_file)
+# Dist = json.load(Dist_file)
+#
+# budget = 287932
+# start = '1'
+# end = '50'
+#
+# def run(c):
+#     global G,Coord,Cost,Dist,budget,start,end
+#     path = [[start],0,0,0]
+#     total_cost = 0
+#     total_dist = 0
+#     deadend = []
+#
+#     path_found = False
+#     priority_queue = [[[start],0,0,0]] #[path,total_cost,total_dist,f(n)]
+#
+#     visited = []
+#
+#     while len(priority_queue) >0 and (not path_found):
+#         current = priority_queue[len(priority_queue)-1]
+#         priority_queue.remove(current)
+#         if len(current[0])>1:
+#             visited.append(current[0][-1])
+#
+#
+#         for path_node in G[current[0][-1]]:
+#             if path_node == end:
+#                 current[1] += Cost[current[0][-1]+','+path_node]
+#                 current[2] += Dist[current[0][-1] + ',' + path_node]
+#                 current[0].append(path_node)
+#                 path = current
+#                 path_found = True
+#             elif len(G[path_node])>1 and path_node not in current[0] and current[1]+Cost[current[0][-1]+','+path_node]<=budget and path_node not in visited:
+#                 new_path = [current[0]+[path_node],current[1]+Cost[current[0][-1]+','+path_node],current[2]+Dist[current[0][-1]+','+path_node]]
+#                 h = math.sqrt((Coord[current[0][-1]][0] - Coord[path_node][0]) ** 2 + (Coord[current[0][-1]][1] - Coord[path_node][1]) ** 2)
+#                 new_path.append(h*c + new_path[1] + new_path[2])
+#                 if len(priority_queue) == 0:
+#                     priority_queue.append(new_path)
+#
+#                 else:
+#                     for i in range(len(priority_queue)):
+#                         if priority_queue[i][3]<=new_path[3]:
+#                             priority_queue.insert(i,new_path)
+#                             break
+#                         elif i == len(priority_queue)-1:
+#                             priority_queue.append(new_path)
+#
+#
+#
+#     return path[2]
+#
+# best_c = 0
+# lowest = 1000000000
+# for c in range(0,30):
+#     result = run(c/10)
+#     if result<lowest and result>0:
+#         best_c = c
+#         lowest = result
+#
+# print(best_c,lowest)
+# #96 158807.5169069521
---
+++ task2.py
@@ -0,0 +1,172 @@
+import json
+from collections import defaultdict
+
+# Open and read json files
+with open('G.json') as f:
+    graph_dict = json.load(f)
+
+with open('Dist.json') as f:
+    edge_dist = json.load(f)
+
+with open('Cost.json') as f:
+    edge_cost = json.load(f)
+
+# Create a Graph class to add attributes
+# Graph class contains a dictionary that maps vertex names to vertex objects.
+class Graph():
+    def __init__(self):
+        """
+        initializes a graph object
+            If no dictionary or None is given,
+            an empty dictionary will be used
+        """
+
+        # Initialise neighbour nodes as a list
+        self.edges = defaultdict(list)
+
+        # Edge distance between 2 nodes
+        self.weights = {}
+
+        # Energy cost between 2 nodes
+        self.costs = {}
+
+    # Function to add an edge
+    # source_node = from_node, dest_node = to_node
+    def add_edge(self, source_node, dest_node, weight, cost):
+        self.edges[source_node].append(dest_node)
+        self.edges[dest_node].append(source_node)
+
+        self.weights[(source_node, dest_node)] = weight
+        self.weights[(dest_node, source_node)] = weight
+
+        self.costs[(source_node, dest_node)] = cost
+        self.costs[(dest_node, source_node)] = cost
+
+# Initialise a graph object
+graph = Graph()
+
+# Add attributes to the created graph object ('source_node', 'dest_node', 'weight', 'cost')
+counter = len(edge_dist)
+for i in edge_dist:
+    # each object in dict is a pair of pair nodes, so counter is needed to avoid duplicate entries
+    if counter % 2 == 0:
+        # Get source and destination node (from and to)
+        source_and_dest = i.split(',') # source_and_dest[0] = source, source_and_dest[1] = destination
+        graph.add_edge(source_and_dest[0], source_and_dest[1], edge_dist[i], edge_cost[i])
+    counter -= 1
+
+energy_budget = 287932
+
+def task2_search(start_node: str, end_node: str, budget: int):
+    # Energy (Edge Cost) budget
+    energy_cost_budget = budget
+
+    # Stores visited nodes with unvisited neighbour nodes
+    unvisited_neighbour = set()
+    unvisited_neighbour.add(start_node)
+
+    # Stores visited nodes with all neighbour nodes visited
+    all_nodes_visited = set()
+
+    # Stores accumulated path cost g(n) from start node to current node (distance)
+    total_distance = {}
+    total_distance[start_node] = 0
+
+    # Stores accumulated energy cost from start node to current node
+    total_energy = {}
+    total_energy[start_node] = 0
+
+    # Stores temp current node
+    temp_node = {}
+    temp_node[start_node] = start_node
+
+    # Queue to pop elements
+    queue = []
+    # insert the starting index
+    queue.append(start_node)
+
+    while len(queue) > 0:
+
+        selected_node = None
+
+        for node in queue:
+            if selected_node == None or total_distance[node] < total_distance[selected_node]:
+                selected_node = node
+
+        if selected_node == None:
+            print("Error!")
+            return
+
+        if selected_node == end_node:
+            shortest_distance_updated = total_distance[selected_node]
+            total_energy_updated = total_energy[selected_node]
+
+            shortest_path = []
+
+            while temp_node[selected_node] != selected_node:
+                shortest_path.append(selected_node)
+                selected_node = temp_node[selected_node]
+
+            shortest_path.append(start_node)
+            shortest_path.reverse()
+
+            print("Shortest path: ", end="")
+            for i in shortest_path:
+                if i != end_node:
+                    print(i, end="->")
+                else:
+                    print(i)
+            print("Shortest distance: ", shortest_distance_updated)
+            print("Total energy cost: ", total_energy_updated)
+            return
+
+        # calculate energy budget and path cost for each neighbour node of the current node
+        for connect_node in graph.edges[selected_node]:
+
+            if connect_node not in unvisited_neighbour and connect_node not in all_nodes_visited:
+                total_energy[connect_node] = total_energy[selected_node] + graph.costs[
+                    connect_node, selected_node]
+
+                if total_energy[connect_node] > energy_cost_budget:
+                    continue
+
+                queue.append(connect_node)
+                unvisited_neighbour.add(connect_node)
+                temp_node[connect_node] = selected_node
+                total_distance[connect_node] = total_distance[selected_node] + graph.weights[connect_node, selected_node]
+
+            # if neighbour node is in the unvisited or visited
+            else:
+
+                if total_distance[connect_node] > total_distance[selected_node] + graph.weights[connect_node, selected_node]:
+                    total_distance[connect_node] = total_distance[selected_node] + graph.weights[connect_node, selected_node]
+                    total_energy[connect_node] = total_energy[selected_node] + graph.costs[
+                        connect_node, selected_node]
+
+                    if total_energy[connect_node] > energy_cost_budget:
+                        continue
+
+                    temp_node[connect_node] = selected_node
+
+                    if connect_node in all_nodes_visited:
+                        all_nodes_visited.remove(connect_node)
+                        queue.append(connect_node)
+                        unvisited_neighbour.add(connect_node)
+
+        # Pop node from queue
+        queue.remove(selected_node)
+
+        # remove current node from unvisited and add to visited
+        unvisited_neighbour.remove(selected_node)
+        all_nodes_visited.add(selected_node)
+
+    print("Invalid path")
+    return
+
+#task2_search("1","50", energy_budget)
+
+
+
+
+
+
